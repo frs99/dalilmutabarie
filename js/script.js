7729a1fcc997ba1cf2affd3874d393fece2f8b4c
frsdjefaflia <djefaflia.fares@gmail.com>
2021-05-07 19:02:51 +0100

    fix js, fix navbar

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
 var startResponsive = document.getElementById("startResponsive");
 var list = document.getElementById("list");
-var open = document.getElementsByClassName("open");
-var close = document.getElementsByClassName("close");
-    document.getElementById("list").style.heigh
+var open = document.getElementsByClassName("open")[0];
+var close = document.getElementsByClassName("close")[0];
+var clickList = document.getElementsByClassName("click-list");
+
+//for open and close navbar with responsive, and show and hide the icons
     startResponsive.onclick = function(){
-        if (document.getElementById("list").style.height === "0px" || document.getElementById("list").style.height === ""){
-            document.getElementById("list").style.height = "auto";
-            document.getElementsByClassName("open")[0].style.display = "none";
-            document.getElementsByClassName("close")[0].style.display = "block"
+        if (list.style.maxHeight === "0px" || list.style.maxHeight === ""){
+            list.style.maxHeight = "236px";
+            open.style.display = "none";
+            close.style.display = "block"
         }
-        else if (document.getElementById("list").style.height = "auto"){
-            document.getElementById("list").style.height = "0";
-            document.getElementsByClassName("open")[0].style.display = "block";
-            document.getElementsByClassName("close")[0].style.display = "none"
+        else if (list.style.maxHeight = "236px"){
+            list.style.maxHeight = "0";
+            open.style.display = "block";
+            close.style.display = "none"
         };
+    };
+
+//for hide navebar whin click
+    for( i = 0; i <= clickList.length; i++){
+        clickList[i].onclick = function(){
+            list.style.maxHeight = "0";
+            open.style.display = "block";
+            close.style.display = "none"
+        }
     };
